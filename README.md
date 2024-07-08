82c895a23a573491c79f99d4a767852fd05994b9
akilgour350 <akilgour350@gmail.com>
2024-07-08 20:12:18 +0100

    let the content flow

--- README.md
+++
@@ -1,3 +0,0 @@
-# akilgour350.github.io
-This is my personal website.
-It's currently in the process of being completely revamped.
--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function setNavbarOpacity() {
     let banner = document.getElementById('banner')
     if(window.scrollY <= banner.offsetHeight - (banner.offsetHeight / 5)){
-        document.getElementById('nav').style.opacity = '0.5';
+        document.getElementById('nav').style.opacity = '0.7';
     } else {
         document.getElementById('nav').style.opacity = '1';
     }
