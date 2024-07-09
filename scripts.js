0c6787f87b53dd08e9afa8990d858dd82c1890f6
akilgour350 <akilgour350@gmail.com>
2024-07-09 19:58:07 +0100

    the website is unleashed

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-function setNavbarOpacity() {
-    let banner = document.getElementById('banner')
-    if(window.scrollY <= banner.offsetHeight - (banner.offsetHeight / 5)){
-        document.getElementById('nav').style.opacity = '0.7';
-    } else {
-        document.getElementById('nav').style.opacity = '1';
-    }
-}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+} 
